e87501842bbf3cab9ae9554a6bdee9a0069b5ce3
Lucas Motta <lucas99motta@hotmail.com>
2021-12-16 22:43:55 -0300

    sistemaDeCadastro

---
+++ cadastro.js
@@ -0,0 +1,27 @@
+//Retirei os comentários para melhora a leitura do código.
+let lista = ["Lucas","Leo","Joana"];
+let vagas = 100
+
+    if(vagas > lista.length){
+        console.log("Cadastro concluído!")
+    }else{
+        console.log("Não temos mais vagas!")
+    }
+// sistema de idade
+ let idadePermitida = 18 
+ let idade 
+// //vamos criar a interação com o usuário, para obter a idade e guadar o valor na variável idade.
+ if(idade < idadePermitida){
+     console.log("Proibido para menores de 18 anos!")
+ }else{
+     console.log("[Parabéns!] Cadastro concluído!")
+ }
+//Sistema de data
+ let data = new Date("2021-12-15")
+ let cadastro = new Date()
+
+if(cadastro > data){
+    console.log("Cadatro negado!")
+ }else{
+     console.log("Cadastro concluido!")
+ }
